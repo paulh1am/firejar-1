2184602b37fad2b01d571ea76bb58aa8a1a9c663
paulh1am <pshiam@gmail.com>
2016-04-11 11:00:13 -0400

    added function for dist?

--- public/js/main.js
+++ public/js/main.js
@@ -24,7 +24,7 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 socket = io.connect();
 
 var marker = "~";
-var mappzy = [];
+ mappzy = [];
 
 function success(pos) {
     crd = pos.coords;
