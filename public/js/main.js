3f03ae8596bef7ae7adcd5feca01eee9ecee7f9d
paulh1am <pshiam@gmail.com>
2016-04-28 10:04:01 -0400

    some front end

--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,8 @@ $( document ).ready(function() {
 
        map = L.map('map').setView([40.75,-74.0059], 12);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'ph1am.io3m04c0',
+    id: 'ph1am.pppj01g0',
     accessToken: 'pk.eyJ1IjoicGgxYW0iLCJhIjoiV01wMkVDQSJ9.HGSWGdj2lTGJLxMcg4C9mA'
   }).addTo(map);
 
